4136fd53ff776d69e2c4e7dd6bbfe3a8ab8cfd0e
Cyber Knight <cyberknight755@gmail.com>
2023-09-09 15:29:30 +0800

    sm6150-common: Drop msm_irqbalance and config
    - We are using SBalance hence msm_irqbalance shouldn't be used.
    
    Change-Id: Ia0a81092c9c93305b6584b7cf776ae63663ab0d5
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -203,10 +203,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/security/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
 
-# IRQ balance
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
