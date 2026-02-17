d63e594030470bcd69977e5283b2b2c146bbe88d
Michael Bestas <mkbestas@lineageos.org>
2026-02-17 15:03:27 +0530

    sm6150-common: Enable charging control bypass mode

--- sm6150.mk
+++ sm6150.mk
@@ -186,8 +186,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
     android.hardware.health-service.qti_recovery
 
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
-
 # Init scripts
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
@@ -213,11 +211,6 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
-
-$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
-$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
