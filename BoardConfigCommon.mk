0451db12cd5d47c8ec23bde0d8068ba73e5adc29
tillua467 <128046674+tillua467@users.noreply.github.com>
2026-08-25 19:36:05 +0600

    Revert "sm6150-common: Use legacy libion implementation"
    This reverts commit adcb74b45d0de691a6c40527af50e609b909222f.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -123,7 +123,6 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
-include device/lineage/sepolicy/libion/sepolicy.mk
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
